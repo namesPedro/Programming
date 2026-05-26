23f50a9f5e97b4f426bc9054e58c0715ac058420
Pedro Radriges <impedro@mail.ru>
2026-05-27 02:15:19 +0700

    completed LAB4

---
+++ src/Contacts/View/Controls/ContactControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace View.Controls
+{
+	public partial class ContactControl : UserControl
+	{
+		public ContactControl()
+		{
+			InitializeComponent();
+		}
+
+		// Запрет ввода символов с клавиатуры
+		private void PhoneTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			// Разрешаем только цифры, пробелы и символы + - ( )
+			e.Handled = !Regex.IsMatch(e.Text, @"^[\d\+\-\(\)\s]$");
+		}
+
+		// Запрет вставки из буфера обмена
+		private void PhoneTextBox_DataObjectPasting(object sender, DataObjectPastingEventArgs e)
+		{
+			if (e.DataObject.GetDataPresent(DataFormats.Text))
+			{
+				string text = (string)e.DataObject.GetData(DataFormats.Text);
+				if (!Regex.IsMatch(text, @"^[\d\+\-\(\)\s]*$"))
+				{
+					e.CancelCommand();
+				}
+			}
+			else
+			{
+				e.CancelCommand();
+			}
+		}
+	}
+}
